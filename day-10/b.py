2c155428dfe7acf6d636cccc38278774c8870cdd
jerryxu20 <xu_jerry@outlook.com>
2024-12-09 22:10:34 -0700

    day 10

---
+++ day-10/b.py
@@ -0,0 +1,45 @@
+import sys
+
+grid = []
+
+for s in sys.stdin:
+    s = s.strip()
+    grid.append(list(map(int, s)))
+
+
+pts = []
+
+n = len(grid)
+m = len(grid[0])
+
+for i in range(n):
+    for j in range(m):
+        pts.append((grid[i][j], i, j))
+
+pts.sort()
+pts = pts[::-1]
+
+delta = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+dp = [[0 for _ in range(m)] for _ in range(n)]
+
+for h, i, j in pts:
+    if h == 9:
+        dp[i][j] = 1
+        continue
+    
+    for a, b in delta:
+        ii = i + a
+        jj = j + b
+        if ii < 0 or jj < 0 or ii >= n or jj >= m: continue
+        if grid[ii][jj] != grid[i][j] + 1: continue
+        dp[i][j] += dp[ii][jj]
+        
+ans = 0
+for i in range(n):
+    for j in range(m):
+        if grid[i][j] == 0:
+            ans += dp[i][j]
+print(ans)
+        
+        
